e19b7ccc0a29f28bb9a55e1992d4cc3aa2ee3db9
ALarmitou <arnaudlarmitou@outlook.fr>
2018-02-12 13:32:29 +0100

    hello world

---
+++ 2018_01_30_v0.1_VAPORWAVE/Assets/Scripts/bougerbidon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bougerbidon : MonoBehaviour
+{
+    
+    private SpriteRenderer spriteRenderer;
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal");
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+    }
+
+
+}
